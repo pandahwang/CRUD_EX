3654deaee42cfc9149d8d7fcd671bff74422b4e3
pandahwang <pandahwang@naver.com>
2024-07-31 12:33:14 +0900

    init: upload

---
+++ src/main/java/com/panda/CRUD/CRUD_EX/CrudExApplication.java
@@ -0,0 +1,13 @@
+package com.panda.CRUD.CRUD_EX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panda/CRUD/CRUD_EX/CrudExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.CRUD.CRUD_EX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
